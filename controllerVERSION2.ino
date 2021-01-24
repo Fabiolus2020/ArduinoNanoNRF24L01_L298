0ea73fbf3c753605e7e8b9a340a9063edcac797c
Fabiolus2020 <76980761+Fabiolus2020@users.noreply.github.com>
2021-01-24 17:13:12 -0500

    Update controllerVERSION2.ino

--- controllerVERSION2.ino
+++ controllerVERSION2.ino
@@ -1,33 +1,49 @@
 /*
-* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
-*/
-#include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
-RF24 radio(8, 9); // CE, CSN
-const byte address[6] = "00001";
+ * If you use the serial monitor for debugging remember to ster the baud rate to 115200.
+ * The modules are set to only one-way communication. That means that the controller can only transmit and the tank can only receive.
+ * If you wish to change this for two-way communication there is plenty of documentation to do so on the internet and in examples.
+ * 
+ * This program reads the analog input of the joystick and sends it in packets to te tank.
+ */
+#include <SPI.h>  
+#include "RF24.h"
 
-char xyData[32] = "";
-String xAxis, yAxis;
-void setup() {
-  Serial.begin(9600);
-  radio.begin();
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
+RF24 myRadio (7, 8);
+byte addresses[][6] = {"0"};
+
+struct package
+{
+  int X=1;
+  int Y=1;
+};
+
+
+typedef struct package Package;
+Package data;
+
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(100);
+  myRadio.begin();  
+  myRadio.setChannel(115); 
+  myRadio.setPALevel(RF24_PA_MIN);
+  myRadio.setDataRate( RF24_250KBPS ) ; 
+  myRadio.openWritingPipe( addresses[0]);
+  delay(100);
 }
-void loop() {
-  
-  xAxis = analogRead(A0); // Read Joysticks X-axis
-  yAxis = analogRead(A1); // Read Joysticks Y-axis
-  // X value
-  xAxis.toCharArray(xyData, 5); // Put the String (X Value) into a character array
-  radio.write(&xyData, sizeof(xyData)); // Send the array data (X value) to the other NRF24L01 modile
-  // Y value
-  yAxis.toCharArray(xyData, 5);
-  radio.write(&xyData, sizeof(xyData));
-  delay(20);
 
-  Serial.print("Y: ");
-  Serial.println(yAxis);
+void loop()
+{
+ 
+  myRadio.write(&data, sizeof(data)); 
+
+  Serial.print("X:");
+  Serial.print(data.X);
+  Serial.print("    Y");
+  Serial.println(data.Y);
+  data.X = analogRead(A0);
+  data.Y = analogRead(A1);
+  delay(100);
 }
