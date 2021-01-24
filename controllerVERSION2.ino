c6abf812a055735857b9d4ad92501d69ffc0f30e
Fabiolus2020 <76980761+Fabiolus2020@users.noreply.github.com>
2021-01-24 16:28:25 -0500

    Update and rename controllerVERSION3.ino to controllerVERSION2.ino

--- controllerVERSION2.ino
+++ controllerVERSION2.ino
@@ -1,9 +1,4 @@
 /*
-* Arduino Wireless Communication Tutorial
-*     Example 1 - Transmitter Code
-*                
-* by Dejan Nedelkovski, www.HowToMechatronics.com
-* 
 * Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
 */
 #include <SPI.h>
